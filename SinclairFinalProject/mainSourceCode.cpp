d9c459dbadff3eeffeafec9592a11f160292d770
Jeremiah-Byrd <101752111+Jeremiah-Byrd@users.noreply.github.com>
2022-04-30 10:01:28 -0400

    Completed Assignment #1

--- SinclairFinalProject/mainSourceCode.cpp
+++ SinclairFinalProject/mainSourceCode.cpp
@@ -37,6 +37,7 @@ void deletingAnAnimal(deque<SeaAnimal>&, deque<LandAnimal>&, deque<Amphibian>&);
 void comparingAnimals(deque<SeaAnimal>&, deque<LandAnimal>&, deque<Amphibian>&);
 void printingAllData(deque<SeaAnimal>&, deque<LandAnimal>&, deque<Amphibian>&);
 void printingSpecificSpecies(deque<SeaAnimal>&, deque<LandAnimal>&, deque<Amphibian>&);
+void printNumberOfAnimalsInProgram(deque<SeaAnimal>&, deque<LandAnimal>&, deque<Amphibian>&);
 void printingALandAnimal(string&, deque<LandAnimal>&);
 void printingASeaAnimal(string&, deque<SeaAnimal>&);
 void printingAnAmphibian(string &, deque<Amphibian> &);
@@ -91,16 +92,15 @@ int main() {
 
 			break;
 		case 5:
-			//Export binary file of animal information 
-			exportingData(seaAnimalRecords, landAnimalRecords, AmphibianRecords);
-
-			break;
-		case 6:
 			//change animals information 
 			changeAnimalRecord(seaAnimalRecords, landAnimalRecords, AmphibianRecords);
 
 			break;
+		case 6:
+			//Printing number of animals in the program 
+			printNumberOfAnimalsInProgram(seaAnimalRecords, landAnimalRecords, AmphibianRecords);
 
+			break;
 		case 7:
 			//Export the file information and close the program 
 			exportingData(seaAnimalRecords, landAnimalRecords, AmphibianRecords);
@@ -217,15 +217,15 @@ int getMenuChoice()
 	cout << "\n2.Delete An Animal";
 	cout << "\n3.Compare Animals";
 	cout << "\n4.View an animal(s) information";
-	cout << "\n5.Export an animal(s) information";
-	cout << "\n6.Change an animal's records";
+	cout << "\n5.Change an animal's records";
+	cout << "\n6.Number of animals in the program";
 	cout << "\n7.End program and save animals to a file";
 
 	//Getting the menu choice 
 	cout << "\n\nEnter Menu Choice: ";
 	while (!(cin >> userInputtedMenuChoice)||cin.fail()||userInputtedMenuChoice < 1 || userInputtedMenuChoice > 7)
 	{
-		cout << "\nERROR: Please input a menu choice that is between 1-6 ";
+		cout << "\nERROR: Please input a menu choice that is between 1-7 ";
 		cin.clear();
 		cin.ignore();
 		cout << "\n\nEnter Menu Choice: ";
@@ -571,10 +571,10 @@ void viewInfo(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAnimals, dequ
 	int speciesSelected = 9;
 
 	//Menu for animl adding options
-	cout << "\nWhat type of animal would you like to add: \n";
-	cout << "\n1. Specific Animal";
+	cout << "\nWhat type of animal would you like to view: \n";
+	cout << "\n1. All Records";
 	cout << "\n2. Entire Species";
-	cout << "\n3. All records\n";
+	cout << "\n3. Specific Animal\n";
 
 	//Asking user for menu choice
 	cout << "\nEnter menu choice: ";
@@ -813,7 +813,7 @@ void changeAnimalRecord(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAni
 			selectingMenuOptionForSeaAnimals(foundPosition,seaAnimals);
 		}
 		else {
-			cout << "\nERROR: Unable to find animal";
+			cout << "\nERROR: Unable to find animal\n";
 		}
 		
 		break;
@@ -828,7 +828,7 @@ void changeAnimalRecord(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAni
 			selectingMenuOptionForLandAnimals(foundPosition, landAnimals);
 		}
 		else {
-			cout << "\nERROR: Unable to find animal";
+			cout << "\nERROR: Unable to find animal\n";
 		}
 
 		break;
@@ -843,7 +843,7 @@ void changeAnimalRecord(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAni
 			selectingMenuOptionForAmphibians(foundPosition, amphibians);
 		}
 		else {
-			cout << "\nERROR: Unable to find animal";
+			cout << "\nERROR: Unable to find animal\n";
 		}
 
 		break;
@@ -851,35 +851,13 @@ void changeAnimalRecord(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAni
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /********************
 PURPOSE:
-delete an animal
+compare animals
 ********************/
-void deletingAnAnimal(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAnimals, deque<Amphibian>& amphibians)
+void printNumberOfAnimalsInProgram(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAnimals, deque<Amphibian>& amphibians)
 {
-	
+	cout << "\nThe number of animals in this program is: " << (seaAnimals.size() + landAnimals.size() + amphibians.size()) << "\n\n";
 }
 
 /********************
@@ -888,12 +866,111 @@ compare animals
 ********************/
 void comparingAnimals(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAnimals, deque<Amphibian>& amphibians)
 {
+	//Variables 
+	int ARRAYSIZE = 1;
+	string animalsBeingCompared[2];
+	int animalsSpecies[2] = {0,2};
+	
 
+	//Getting the animals Name 
+	animalsBeingCompared[0] = initializingStringVariable("\nWhat is the first animal you want to compare: ");
+	animalsBeingCompared[1] = initializingStringVariable("\nWhat is the second animal you want to compare: ");
+
+	//Getting the species of the animal 
+	while (animalsSpecies[0] > 3 || animalsSpecies[0] < 1 || animalsSpecies[1] > 3 || animalsSpecies[1] < 1)
+	{
+		cout << "\nPLEASE ENTER: 1 for Sea Animal, 2 for Land Animal, 3 for Amphibians";
+		animalsSpecies[0] = initializingIntVariable("\nWhat species is animal one: ");
+		animalsSpecies[1] = initializingIntVariable("\nWhat species is animal two: ");
+	}
+
+	//Finding where this animal is found 
+	for (int i = 0; i < 2; i++) {
+
+		//Looking for the animal location
+		switch (animalsSpecies[i])
+		{
+		case 1:
+			//Printing the animal 
+			printingASeaAnimal(animalsBeingCompared[i], seaAnimals);
+
+			break;
+		case 2:
+			//Printing the animal 
+			printingALandAnimal(animalsBeingCompared[i], landAnimals);
+
+			break;
+		case 3:
+			//Printing the animal 
+			printingAnAmphibian(animalsBeingCompared[i], amphibians);
+
+			break;
+		}
+	}
 }
 
+/********************
+PURPOSE:
+delete an animal
+********************/
+void deletingAnAnimal(deque<SeaAnimal>& seaAnimals, deque<LandAnimal>& landAnimals, deque<Amphibian>& amphibians)
+{
+	//Variables 
+	string animalName;
+	int speciesOfAnimal = 4;
+	int foundPosition = -1;
 
+	//Asking the the user for the animal
+	animalName = initializingStringVariable("\nWhat is the animal's name: ");
 
+	//What species is it apart of 
+	while (speciesOfAnimal > 3 || speciesOfAnimal < 1 || speciesOfAnimal > 3 || speciesOfAnimal < 1)
+	{
+		cout << "\nPLEASE ENTER: 1 for Sea Animal, 2 for Land Animal, 3 for Amphibians";
+		speciesOfAnimal = initializingIntVariable("\nWhat species is this animal: ");
+	}
 
+	//Finding the species specific code 
+	switch (speciesOfAnimal)
+	{
+	case 1:
+		//Printing the animal 
+		foundPosition = searchingForASeaAnimal(animalName, seaAnimals);
 
+		//Deleting if not negative
+		if (foundPosition > -1)
+		{
+			//Delete Class
+			SeaAnimal* animalLocationPointer = &(seaAnimals[foundPosition]);
+			delete animalLocationPointer;
+		}
 
+		break;
+	case 2:
+		//Printing the animal 
+		foundPosition = searchingForALandAnimal(animalName, landAnimals);
 
+		//Deleting if not negative
+		if (foundPosition > -1)
+		{
+			//Deleting Class
+			LandAnimal* animalLocationPointer = &(landAnimals[foundPosition]);
+			delete animalLocationPointer;
+		}
+
+		break;
+	case 3:
+		//Printing the animal 
+		foundPosition = searchingForAnAmphibian(animalName, amphibians);
+
+		//Deleting if not negative
+		if (foundPosition > -1)
+		{
+			//Deleting Class
+			Amphibian* animalLocationPointer = &(amphibians[foundPosition]);
+			delete animalLocationPointer;
+		}
+
+		break;
+	}
+}
